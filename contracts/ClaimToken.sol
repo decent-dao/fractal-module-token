63c49481297a97defff190655cf163a539090ed2
Christopher Dancy <chris.dancy@decentlabs.io>
2022-07-14 18:27:38 +0200

    updated data types

--- contracts/ClaimToken.sol
+++ contracts/ClaimToken.sol
@@ -1,3 +1,4 @@
+//SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 import "./VotesToken.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
@@ -6,13 +7,17 @@ import "./TokenFactory.sol";
 contract ClaimToken {
     using SafeERC20 for IERC20;
     
-    mapping(address => uint) public tokenSnapId;
-    mapping(address => mapping(address => bool)) public claimed;
+    struct ChildTokenInfo{
+        uint snapId;
+        mapping(address => bool) claimed;
+    }
+    mapping(address => mapping(address => ChildTokenInfo)) public cTokens; // pToken => cToken => snapId
+    mapping(address => address) public findMyDaddy;
 
     // how should the token even be created?
     // where do the tokens live? DAO or in claim contract
     // how to update for vesting functionality
-    function addTokenDrop(address factory, address ptoken) public returns(address cToken) {
+    function addTokenDrop(address factory, address pToken) public returns(address cToken) {
         bytes[] memory tokenBytes = new bytes[](5);
 
         tokenBytes[0] = abi.encode("name");
@@ -22,24 +27,26 @@ contract ClaimToken {
         tokenBytes[4] = abi.encode("salt");
 
         cToken = TokenFactory(factory).create(msg.sender, tokenBytes)[0]; // create Token & create Hash
-        tokenSnapId[ptoken] = VotesToken(ptoken).captureSnapShot();
+        cTokens[pToken][cToken].snapId = VotesToken(pToken).captureSnapShot();
+        findMyDaddy[cToken] = pToken;
     }
 
-    function claim(address token, address claimer) public {
-        require(!claimed[token][claimer], "This allocation has been claimed");
-        claimed[token][claimer] = true; // house keeping
+    function claim(address cToken, address claimer) public {
+        address pToken = findMyDaddy[cToken];
+        require(cTokens[pToken][cToken].claimed[claimer], "This allocation has been claimed");
+        cTokens[findMyDaddy[cToken]][cToken].claimed[claimer] = true; // house keeping
 
-        uint amount = calculateClaimAmount(token, claimer); // Get user balance
-        IERC20(token).safeTransfer(claimer, amount); // transfer user balance
+        uint amount = calculateClaimAmount(pToken, cToken, claimer); // Get user balance
+        IERC20(cToken).safeTransfer(claimer, amount); // transfer user balance
     }
 
-    function calculateClaimAmount(address token, address claimer) public view returns(uint userCTokenAllocation) {
+    function calculateClaimAmount(address pToken, address cToken, address claimer) public view returns(uint userCTokenAllocation) {
         uint childTokenSupply;
         uint percentParentAllocation;
         uint pHoldersCTokenAllocation = percentParentAllocation * childTokenSupply; // total tokens avialable to pToken holders
 
-        uint parentTokenSupply = VotesToken(token).totalSupplyAt(tokenSnapId[token]);
-        uint userPTokenBalance = VotesToken(token).balanceOfAt(claimer, tokenSnapId[token]);
+        uint parentTokenSupply = VotesToken(pToken).totalSupplyAt(cTokens[pToken][cToken].snapId);
+        uint userPTokenBalance = VotesToken(pToken).balanceOfAt(claimer, cTokens[pToken][cToken].snapId);
         uint userToTotalSupply = userPTokenBalance / parentTokenSupply; // user token balance / total supply
 
         userCTokenAllocation = pHoldersCTokenAllocation * userToTotalSupply; // usersCTokenAllocation
