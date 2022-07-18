eada0bafeaaf844c84e9491540f36e5352a1e430
Christopher Dancy <chris.dancy@decentlabs.io>
2022-07-18 16:47:57 +0200

    seperated merkle from snap

--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -21,25 +21,81 @@ contract TokenFactory is ITokenFactory, ERC165 {
 
         string memory name = abi.decode(data[0], (string));
         string memory symbol = abi.decode(data[1], (string));
-        uint totalSupply = abi.decode(data[2], (uint));
+        uint256 totalSupply = abi.decode(data[2], (uint256));
         address claimContract = abi.decode(data[3], (address));
         bytes32 salt = abi.decode(data[4], (bytes32));
-        bytes32 merkleRoot = abi.decode(data[4], (bytes32));
+        bytes32 merkleRoot = abi.decode(data[5], (bytes32));
 
-        createdContracts[0] = Create2.deploy(
+        createdContracts[0] = _createToken(
+            creator,
+            claimContract,
+            salt,
+            merkleRoot,
+            name,
+            symbol,
+            totalSupply
+        );
+
+        return createdContracts;
+    }
+
+    function createWSnap(address creator, bytes[] calldata data)
+        external
+        returns (address[] memory)
+    {
+        address[] memory createdContracts = new address[](1);
+
+        string memory name = abi.decode(data[0], (string));
+        string memory symbol = abi.decode(data[1], (string));
+        uint256 totalSupply = abi.decode(data[2], (uint256));
+        address claimContract = abi.decode(data[3], (address));
+        bytes32 salt = abi.decode(data[4], (bytes32));
+        bytes32 merkleRoot = abi.decode(data[5], (bytes32));
+
+        // create Snapshot
+        address pToken = abi.decode(data[6], (address));
+        uint256 pAllocation = abi.decode(data[7], (uint256));
+
+        createdContracts[0] = _createToken(
+            creator,
+            claimContract,
+            salt,
+            merkleRoot,
+            name,
+            symbol,
+            totalSupply
+        );
+        ClaimToken(claimContract).addSnap(
+            pToken,
+            createdContracts[0],
+            pAllocation
+        );
+
+        return createdContracts;
+    }
+
+    function _createToken(
+        address creator,
+        address claimContract,
+        bytes32 salt,
+        bytes32 merkleRoot,
+        string memory name,
+        string memory symbol,
+        uint256 totalSupply
+    ) internal returns (address createdToken) {
+        createdToken = Create2.deploy(
             0,
-            keccak256(abi.encodePacked(creator, msg.sender, block.chainid, salt)),
+            keccak256(
+                abi.encodePacked(creator, msg.sender, block.chainid, salt)
+            ),
             abi.encodePacked(
                 type(VotesToken).creationCode,
                 abi.encode(name, symbol, totalSupply, claimContract)
             )
         );
-
-        ClaimToken(claimContract).addMerkle(createdContracts[0], merkleRoot);
-
-        emit TokenCreated(createdContracts[0]);
-
-        return createdContracts;
+        ClaimToken(claimContract).addMerkle(createdToken, merkleRoot);
+        emit TokenCreated(createdToken);
+        return createdToken;
     }
 
     /// @notice Returns whether a given interface ID is supported
