1ba504add4502f1834b386fe66f68142ea1341fb
Christopher Dancy <chris.dancy@decentlabs.io>
2022-07-19 18:56:59 +0200

    added events and moved around claim amounts

--- contracts/ClaimToken.sol
+++ contracts/ClaimToken.sol
@@ -23,10 +23,21 @@ contract ClaimToken {
     mapping(address => address) public findMyDaddy;
     mapping(address => MerkleInfo) public merkles;
 
-    event MerkleClaimed(address indexed token, address indexed claimer, uint256 amount);
+    event MerkleAdded(address token, bytes32 merkleRoot);
+    event MerkleClaimed(
+        address indexed token,
+        address indexed claimer,
+        uint256 amount
+    );
+    event SnapAdded(address pToken, address cToken, uint256 pAllocation);
+    event SnapClaimed(
+        address indexed pToken,
+        address indexed cToken,
+        address indexed claimer,
+        uint256 amount
+    );
 
     ///////////////// Merkle And Swap //////////////////////////
-
     function batchClaimMerkleAndSnap(
         address[] calldata cToken,
         address[] calldata claimer,
@@ -46,6 +57,7 @@ contract ClaimToken {
             "This token has been initialized with a merkle root"
         );
         merkles[token].merkleRoot = merkleRoot;
+        emit MerkleAdded(token, merkleRoot);
     }
 
     function claimMerkle(
@@ -71,41 +83,31 @@ contract ClaimToken {
         uint256 pAllocation
     ) external returns (uint256 snapId) {
         require(
-            cTokens[pToken][cToken].snapId != 0,
+            cTokens[pToken][cToken].snapId == 0,
             "This token has already been initilized with a snapId"
         );
         snapId = VotesToken(pToken).captureSnapShot();
         cTokens[pToken][cToken].snapId = snapId;
         cTokens[pToken][cToken].pAllocation = pAllocation;
         findMyDaddy[cToken] = pToken;
+        emit SnapAdded(pToken, cToken, pAllocation);
     }
 
     function claimSnap(address cToken, address claimer) public {
         address pToken = findMyDaddy[cToken];
+        uint256 amount = calculateClaimAmount(pToken, cToken, claimer); // Get user balance
+        require(amount > 0, "The claimer does not have an allocation");
         require(
-            cTokens[pToken][cToken].isSnapClaimed[claimer],
+            !cTokens[pToken][cToken].isSnapClaimed[claimer],
             "This allocation has been claimed"
         );
         cTokens[pToken][cToken].isSnapClaimed[claimer] = true;
 
-        uint256 amount = calculateClaimAmount(pToken, cToken, claimer); // Get user balance
         IERC20(cToken).safeTransfer(claimer, amount); // transfer user balance
+        emit SnapClaimed(pToken, cToken, claimer, amount);
     }
 
     //////////////////// View Functions //////////////////////////
-    function calculateClaimAmount(
-        address pToken,
-        address cToken,
-        address claimer
-    ) public view returns (uint256 cTokenAllocation) {
-        cTokenAllocation =
-            (VotesToken(pToken).balanceOfAt(
-                claimer,
-                cTokens[pToken][cToken].snapId
-            ) * IERC20(cToken).totalSupply()) /
-            VotesToken(pToken).totalSupplyAt(cTokens[pToken][cToken].snapId);
-    }
-
     function merkleVerify(
         address token,
         address account,
@@ -119,5 +121,18 @@ contract ClaimToken {
             "MerkleDistributor: Invalid proof."
         );
         success = true;
+    }
+
+    function calculateClaimAmount(
+        address pToken,
+        address cToken,
+        address claimer
+    ) public view returns (uint256 cTokenAllocation) {
+        cTokenAllocation =
+            (VotesToken(pToken).balanceOfAt(
+                claimer,
+                cTokens[pToken][cToken].snapId
+            ) * cTokens[pToken][cToken].pAllocation) /
+            VotesToken(pToken).totalSupplyAt(cTokens[pToken][cToken].snapId);
     }
 }
