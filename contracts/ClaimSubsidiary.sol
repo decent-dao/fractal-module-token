e79776b9b81745e7c5d992d48eb2bfeed8b9fa12
Christopher Dancy <chris.dancy@decentlabs.io>
2022-07-20 13:39:31 +0200

    removed merkle structure and updated name

--- contracts/ClaimSubsidiary.sol
+++ contracts/ClaimSubsidiary.sol
@@ -5,9 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "./TokenFactory.sol";
 
-// todo: add vesting ability
-//todo: remove liquidity if not claimed
-contract ClaimToken {
+contract ClaimSubsidiary {
     using SafeERC20 for IERC20;
 
     struct ChildTokenInfo {
@@ -15,20 +13,9 @@ contract ClaimToken {
         uint256 pAllocation;
         mapping(address => bool) isSnapClaimed;
     }
-    struct MerkleInfo {
-        bytes32 merkleRoot;
-        mapping(address => bool) isMerkleClaimed;
-    }
     mapping(address => mapping(address => ChildTokenInfo)) public cTokens; // pToken => cToken => snapId
     mapping(address => address) public findMyDaddy;
-    mapping(address => MerkleInfo) public merkles;
 
-    event MerkleAdded(address token, bytes32 merkleRoot);
-    event MerkleClaimed(
-        address indexed token,
-        address indexed claimer,
-        uint256 amount
-    );
     event SnapAdded(address pToken, address cToken, uint256 pAllocation);
     event SnapClaimed(
         address indexed pToken,
@@ -36,45 +23,6 @@ contract ClaimToken {
         address indexed claimer,
         uint256 amount
     );
-
-    ///////////////// Merkle And Swap //////////////////////////
-    function batchClaimMerkleAndSnap(
-        address[] calldata cToken,
-        address[] calldata claimer,
-        uint256[] calldata amount,
-        bytes32[][] calldata merkleProof
-    ) external {
-        for (uint256 i; i < cToken.length; i++) {
-            claimMerkle(cToken[i], claimer[i], amount[i], merkleProof[i]);
-            claimSnap(cToken[i], claimer[i]);
-        }
-    }
-
-    ///////////////////// Merkle ///////////////////////////////////////////
-    function addMerkle(address token, bytes32 merkleRoot) external {
-        require(
-            merkles[token].merkleRoot == "",
-            "This token has been initialized with a merkle root"
-        );
-        merkles[token].merkleRoot = merkleRoot;
-        emit MerkleAdded(token, merkleRoot);
-    }
-
-    function claimMerkle(
-        address token,
-        address claimer,
-        uint256 amount,
-        bytes32[] calldata merkleProof
-    ) public {
-        require(
-            !merkles[token].isMerkleClaimed[claimer],
-            "This allocation has been claimed"
-        );
-        merkles[token].isMerkleClaimed[claimer] = true;
-        merkleVerify(token, claimer, amount, merkleProof);
-        IERC20(token).safeTransfer(claimer, amount);
-        emit MerkleClaimed(token, claimer, amount);
-    }
 
     ////////////////////////// SnapShot //////////////////////////////////
     function addSnap(
@@ -108,21 +56,6 @@ contract ClaimToken {
     }
 
     //////////////////// View Functions //////////////////////////
-    function merkleVerify(
-        address token,
-        address account,
-        uint256 amount,
-        bytes32[] calldata merkleProof
-    ) public view returns (bool success) {
-        // Verify the merkle proof.
-        bytes32 node = keccak256(abi.encodePacked(account, amount));
-        require(
-            MerkleProof.verify(merkleProof, merkles[token].merkleRoot, node),
-            "MerkleDistributor: Invalid proof."
-        );
-        success = true;
-    }
-
     function calculateClaimAmount(
         address pToken,
         address cToken,
