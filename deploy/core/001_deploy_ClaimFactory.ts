2d344a7bf5e9e7558251787590d7993e1a27df15
Christopher Dancy <chris.dancy@decentlabs.io>
2022-07-26 08:56:03 -0400

    removed init

--- deploy/core/001_deploy_ClaimFactory.ts
+++ deploy/core/001_deploy_ClaimFactory.ts
@@ -5,6 +5,5 @@ import { deployNonUpgradeable } from "../helpers/deployNonUpgradeable";
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   await deployNonUpgradeable(hre, "ClaimFactory", []);
   const claimFactory = await hre.ethers.getContract("ClaimFactory");
-  await claimFactory.initialize();
 };
 export default func;
